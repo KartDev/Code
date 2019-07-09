c8b43a2601560122d7b79603f4f8061508254e3e
kartdev <oskarlidstrom@gmail.com>
2019-07-09 08:41:57 +0200

    Added files

---
+++ JavaScript/Basic Shapes.js
@@ -0,0 +1,15 @@
+// Using basic shapes and colors, make a piece of food appear on the plate 
+
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300);
+
+// pepperoni pizza
+fill(255, 255, 0);
+stroke(145, 97, 0);
+strokeWeight(5);
+triangle(94, 287, 332, 266, 179, 61);
+fill(255, 0, 0);
+ellipse(200, 151, 29, 30);
+ellipse(172, 217, 29, 30);
+ellipse(252, 233, 29, 30);
